7b6c794be4f36e6e4da6bc177d575a912344d102
mirok <miroslav.kuchta@gmail.com>
2022-07-04 14:07:10 +0200

    test poisson

--- poisson.jl
+++ poisson.jl
@@ -14,7 +14,7 @@ function poisson_solver(model, f0, g0, h0, Dirichlet_tags; pdegree)
     @assert !isempty(Dirichlet_tags)
     # Allowed tags are ...
     all_tags = get_boundary_tags(model)
-    @assert all(t ∈ all_tags for t ∈ Dirichlet_tags)
+    #@assert all(t ∈ all_tags for t ∈ Dirichlet_tags) || all(t ∈ [1, 2, 3, 4] for t ∈ Dirichlet_tags)
     Neumann_tags = filter(x -> x ∉ Dirichlet_tags, all_tags)
 
     # Define Dirichlet boundaries
@@ -24,21 +24,28 @@ function poisson_solver(model, f0, g0, h0, Dirichlet_tags; pdegree)
     writevtk(model,"model")
 
     reffe = ReferenceFE(lagrangian, Float64, pdegree)
-    V0 = TestFESpace(model, reffe; conformity=:H1, dirichlet_tags="dirichlet")
+    dirichlet_tags = "dirichlet"
+    V0 = TestFESpace(model, reffe; conformity=:H1, dirichlet_tags=dirichlet_tags)
+
+    ΓD = BoundaryTriangulation(model, tags=dirichlet_tags)
+    dΓD = Measure(ΓD, 0)
+    L = sum(∫(1)*dΓD)
+
+    @show L
 
     Ug = TrialFESpace(V0, g0)
-
+    @show length(Ug.dirichlet_values) norm(Ug.dirichlet_values)
     degree = pdegree+1
     Ω = Triangulation(model)
-    dΩ = Measure(Ω, 2*degree)
+    dΩ = Measure(Ω, 2*(degree-1))
 
     # Bilinear form
     a(u,v) = ∫( ∇(v)⋅∇(u) )*dΩ
 
     Γ = BoundaryTriangulation(model, tags=Neumann_tags)
     ν = get_normal_vector(Γ)
-    dΓ = Measure(Γ, 2*degree)
-    b(v) = ∫( v*f0 )*dΩ + ∫(v*(h0⋅ν))*dΓ
+    dΓ = Measure(Γ, degree+2)
+    b(v) = isempty(Neumann_tags) ? ∫( v*f0 )*dΩ : ∫( v*f0 )*dΩ + ∫(v*(h0⋅ν))*dΓ
 
     one(x) = 1
     !isempty(Neumann_tags) && @assert sqrt(sum( ∫(one)*dΓ )) > 0
@@ -51,16 +58,29 @@ function poisson_solver(model, f0, g0, h0, Dirichlet_tags; pdegree)
     uh = solve(solver, op)
 
     return uh
-end
+end    
+
+
+x = Symbolics.variables(:x, 1:2)
+
+# Manufactured solution
+u0s = Dict(1 => cos(π*(x[1]*x[2])),
+           2 => cos(2*π*x[1])*cos(3*π*x[2]) + 1,
+           3 => sin(2*π*x[1])*sin(3*π*x[2]) + 1,
+           4 => cos(2*π*(x[1]-x[2])),
+           5 => sin(2*π*(x[1]+x[2])))
 
 true && begin
-
-    x = Symbolics.variables(:x, 1:2)
-
-    # Manufactured solution
-    #u0_ = cos(π*(x[1]*x[2]))
-    u0_ = cos(2*π*x[1])*cos(3*π*x[2]) + 1
-    # u0_ = cos(2*π*(x[1]-x[2]))
+    #
+    #
+    # (3) 6 (4)
+    #  7     8
+    # (1) 5 (2)
+    Dirichlet_tags = [1, 2, 3, 4, 5, 6, 7]
+    which = 4
+    pdegree = 3 # Polynomial degree of FE space
+        
+    u0_ = u0s[which]
 
     flux_ = -Grad(u0_)
     f0_ = Div(flux_)
@@ -68,13 +88,9 @@ true && begin
 
     u0, f0, h0 = (compile(arg, x) for arg in (u0_, f0_, h0_))
 
-    Dirichlet_tags = [5, 6]
-
-    pdegree = 3 # Polynomial degree of FE space
-
     global uh
     errors, hs, ndofs = [], [], []
-    for k ∈ 2:6
+    for k ∈ 2:5
         n = 2^(k)
 
         domain = (0,1,0,1)
